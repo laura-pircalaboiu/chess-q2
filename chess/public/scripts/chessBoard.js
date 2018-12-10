aa6ca687f30a7d6b032acea70120c03e96fc3619
redshifts <laura.pircalaboiu@yahoo.it>
2018-12-10 13:47:09 +0100

    kinda snapping

--- chess/public/scripts/chessBoard.js
+++ chess/public/scripts/chessBoard.js
@@ -1,6 +1,6 @@
 
 function chessBoard() {
-    const cb = Array(8).fill(0).map(x => Array(8).fill(0));
+    let cb = Array(8).fill(0).map(x => Array(8).fill(0));
     const cbOrder = [cpType.Rook, cpType.Knight, cpType.Bishop,
     cpType.King, cpType.Queen, cpType.Bishop,
     cpType.Knight, cpType.Rook];
@@ -13,42 +13,40 @@ function chessBoard() {
     for (let r = 0; r < cb.length; r++) {
         for (let c = 0; c < cb.length; c++) {
             let div = document.createElement('div');
-
+            console.log('test')
+           
             if ((r + c) % 2 === 0)
                 div.classList.add('Black');
             else
                 div.classList.add('White');
-
+            
             counter++;
             div.setAttribute('id', (counter) + '');
-
+            div.setAttribute('draggable', "true");
 
             if (r === 0) {
-                cb[r, c] = new chessPiece(cbOrder[c], cpColour.Black, div);
+                div.chessPiece = new chessPiece(cbOrder[c], cpColour.Black, div);
                 div.setAttribute('piece', `${cbOrder[c]}`);
                 div.innerHTML = "bpi"
-                div.setAttribute("draggable",true);
             }
 
             if (r === 1) {
-                cb[r][c] = new chessPiece(cpType.Pawn, cpColour.Black, div);
+        
+                div.chessPiece = new chessPiece(cpType.Pawn, cpColour.Black, div);
                 div.setAttribute('piece', `${cpType.Pawn}`);
                 div.innerHTML = "bp"
-                div.setAttribute("draggable",true);
             }
 
             if (r === 6) {
-                cb[r, c] = new chessPiece(cpType.Pawn, cpColour.White, div);
+                div.chessPiece = new chessPiece(cpType.Pawn, cpColour.White, div);
                 div.setAttribute('piece', `${cpType.Pawn}`)
                 div.innerHTML = "wp"
-                div.setAttribute("draggable",true);
             }
 
             if (r === 7) {
-                cb[r][c] = new chessPiece(cbOrder[c], cpColour.White, div);
+                div.chessPiece = new chessPiece(cbOrder[c], cpColour.White, div);
                 div.setAttribute('piece', `${cbOrder[c]}`);
                 div.innerHTML = "wpi"
-                div.setAttribute("draggable",true);
             }
 
             else
@@ -58,20 +56,33 @@ function chessBoard() {
                 this.style.opacity="100%"
             })
 
-           /* div.addEventListener("dragend",function(e){
-                console.log(e.screenX+" "+e.screenY)
-                let refTop = cbSection.getBoundingClientRect().top
-                let refLeft = cbSection.getBoundingClientRect().left
-                for(let row=0;row<cb.length;row++){
-                    for(let col=0;col<cb.length;col++){
-                        cb[r][c].div.getBoundingClientRect().top
-                        cb[r][c].div.getBoundingClientRect().left
-                        if()
-                    }
-                }
-            })*/
+           
             div.style.top = `calc(12.5% * ${r})` 
             div.style.left = `calc(12.5% * ${c})` 
+
+            div.addEventListener("dragend",function(e){
+                console.log(e.screenX+" "+e.screenY)
+                let minDistance=10e100
+                let newX
+                let newY
+                for(let row=0;row<cb.length;row++){
+                    for(let col=0;col<cb.length;col++){
+                        let currentTop = cb[row][col].getBoundingClientRect().top
+                        let currentLeft = cb[row][col].getBoundingClientRect().left
+                        if(Math.sqrt(Math.abs(e.screenX-currentLeft)+Math.abs(e.screenY-currentTop))<minDistance){
+                            minDistance=Math.sqrt(Math.abs(e.screenX-currentLeft)+Math.abs(e.screenY-currentTop));
+                            //change div to div im holding
+                            newX=row;
+                            newY=col;
+                            console.log("yeet")
+                        }
+                    }
+                }
+                console.log(newX,newY)
+            })
+
+            cb[r][c]=div;
+
             cbSection.appendChild(div);
         }
     }
