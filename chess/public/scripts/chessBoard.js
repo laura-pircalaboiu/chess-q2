2fdc0146771c5fe5c4942b9de98a0f9a5ffc346f
Druue <tileyratkins@gmail.com>
2018-12-10 11:32:03 +0100

    ChessBoard Display

--- chess/public/scripts/chessBoard.js
+++ chess/public/scripts/chessBoard.js
@@ -1,50 +1,58 @@
 
 function chessBoard() {
-        const cb = Array(8).fill(0).map(x => Array(8).fill(0));
-        const cbOrder = [cpType.Rook, cpType.Knight, cpType.Bishop,
-        cpType.King, cpType.Queen, cpType.Bishop,
-        cpType.Knight, cpType.Rook];
+    const cb = Array(8).fill(0).map(x => Array(8).fill(0));
+    const cbOrder = [cpType.Rook, cpType.Knight, cpType.Bishop,
+    cpType.King, cpType.Queen, cpType.Bishop,
+    cpType.Knight, cpType.Rook];
 
-        const cbSection = document.querySelector("#board");
-        for (let r = 0; r < cb.length; r++) {
-            for (let c = 0; c < cb.length; c++) {
-                let div = document.createElement('div');
-                
-                if ((r + c) % 2 === 0)
-                    div.classList.add('Black');
-                else
-                    div.classList.add('White');
+    const cbSection = document.querySelector("#board");
+    console.log(cbSection)
 
-                div.setAttribute('id', '' + (r + c));
+    let counter = 0;
 
-                if (r == 0) {
-                    cb[r, c] = new chessPiece(cbOrder[c], cpColour.Black);
-                    div.setAttribute('piece', `${cbOrder[c]}`);
-                }
-                
-                if (r = 1) {
-                    cb[r][c] = new chessPiece(cpType.Pawn, cpColour.Black);
-                    div.setAttribute('piece', `${cbOrder[c]}`);
-                }
+    for (let r = 0; r < cb.length; r++) {
+        for (let c = 0; c < cb.length; c++) {
+            let div = document.createElement('div');
 
-                if (r == 6)
-                {
-                    cb[r, c] = new chessPiece(cpType.Pawn, cpColour.White);
-                    div.setAttribute('piece', `${cpType.Pawn}`)
-                }
+            if ((r + c) % 2 === 0)
+                div.classList.add('Black');
+            else
+                div.classList.add('White');
 
-                if(r == 7)
-                {
-                    cb[r][c] = new chessPiece(cbOrder[c], cpColour.White);
-                    div.setAttribute('piece', `${cbOrder[c]}`);
-                }
+            counter++;
+            div.setAttribute('id', (counter) + '');
 
-                else
-                    div.setAttribute('piece', 'None');
-                
-                cbSection.appendChild(div);
+
+            if (r === 0) {
+                cb[r, c] = new chessPiece(cbOrder[c], cpColour.Black);
+                div.setAttribute('piece', `${cbOrder[c]}`);
             }
+
+            if (r === 1) {
+                cb[r][c] = new chessPiece(cpType.Pawn, cpColour.Black);
+                div.setAttribute('piece', `${cbOrder[c]}`);
+            }
+
+            if (r === 6) {
+                cb[r, c] = new chessPiece(cpType.Pawn, cpColour.White);
+                div.setAttribute('piece', `${cpType.Pawn}`)
+            }
+
+            if (r === 7) {
+                cb[r][c] = new chessPiece(cbOrder[c], cpColour.White);
+                div.setAttribute('piece', `${cbOrder[c]}`);
+            }
+
+            else
+                div.setAttribute('piece', 'None');
+
+
+            div.style.top = `calc(12.5% * ${r})` 
+            div.style.left = `calc(12.5% * ${c})` 
+            cbSection.appendChild(div);
         }
     }
-    //var cpType = this.cpType;
+}
+// document.onload = function(){
 chessBoard();
+// }
